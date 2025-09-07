67d908e07559784fe02efeb7500688278f8a8c50
Andrewpiza <148090873+Andrewpiza@users.noreply.github.com>
2025-09-07 11:45:47 -0400

    Boosting & Drift Boost

--- Mushroom Kart/Assets/Scripts/Racer.cs
+++ Mushroom Kart/Assets/Scripts/Racer.cs
@@ -7,19 +7,24 @@ public class Racer : MonoBehaviour
 {
     [Header("Movement")]
     [SerializeField] private float acceleration = 1500;
-    [SerializeField] private float maxSpeed = 15;
+    [SerializeField] private float baseMaxSpeed = 15;
 
-    [SerializeField] private bool isDrifting;
+    private float boost;
+
+    // Drift
+    private bool isDrifting;
+    private float driftAngle;
+    private float driftCharge;
 
     private Rigidbody2D rb;
-    private float driftAngle;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
         float xMove = Input.GetAxisRaw("Horizontal");
         float yMove = Input.GetAxisRaw("Vertical");
@@ -28,35 +33,48 @@ public class Racer : MonoBehaviour
         {
             if (driftAngle == 0)
             {
-                if (Vector2.SignedAngle(transform.right, rb.velocity.normalized) > 0) driftAngle = 1;
-                if (Vector2.SignedAngle(transform.right, rb.velocity.normalized) < 0) driftAngle = -1;
+                if (Vector2.SignedAngle(transform.right, rb.velocity.normalized) > 0.25) driftAngle = 1;
+                if (Vector2.SignedAngle(transform.right, rb.velocity.normalized) < -0.25) driftAngle = -1;
             }
-            isDrifting = true;
+            else
+            {
+                isDrifting = true;
+            }
         }
-        else
+        else if (isDrifting == true)
         {
             isDrifting = false;
             driftAngle = 0;
+            Boost(driftCharge);
+            driftCharge = 0;
         }
-        
+
         Move(new Vector2(xMove, yMove));
     }
 
     public void Move(Vector2 move)
     {
+        float maxSpeed = baseMaxSpeed + boost;
         float newAcceleration = acceleration;
 
         if (isDrifting)
         {
-            newAcceleration *= 1.5f;
-            
-            Vector2 driftMove = Quaternion.AngleAxis(45 * driftAngle, Vector3.forward) * transform.right;
+            newAcceleration *= 1.6f;
 
+            Vector2 driftMove = Quaternion.AngleAxis(45 * driftAngle, Vector3.forward) * transform.right;
             move += driftMove;
-        } 
+
+            driftCharge += Time.deltaTime / 4;
+            if (move != Vector2.zero && Vector2.Angle(move, driftMove) < 25) driftCharge += Time.deltaTime / 2;
+        }
+
+        if (boost > 0)
+        {
+            boost -= Time.deltaTime * 2;
+            if (boost < 0) boost = 0;
+        }
 
         rb.AddForce(move * newAcceleration * Time.deltaTime);
-
         if (rb.velocity.magnitude > maxSpeed)
         {
             rb.velocity = rb.velocity.normalized * maxSpeed;
@@ -72,5 +90,12 @@ public class Racer : MonoBehaviour
         float angle = Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
 
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    public void Boost(float b)
+    {
+        boost += b;
+
+        rb.velocity = rb.velocity.normalized * (baseMaxSpeed + boost);
     }
 }
