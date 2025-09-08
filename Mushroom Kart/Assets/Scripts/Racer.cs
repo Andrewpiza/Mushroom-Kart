aba1793e5ac2ad9d067a665078ad17ed9a7c6d90
Andrewpiza <148090873+Andrewpiza@users.noreply.github.com>
2025-09-08 11:37:14 -0400

    Drift Fix & Jumping

--- Mushroom Kart/Assets/Scripts/Racer.cs
+++ Mushroom Kart/Assets/Scripts/Racer.cs
@@ -9,25 +9,37 @@ public class Racer : MonoBehaviour
     [SerializeField] private float acceleration = 1500;
     [SerializeField] private float baseMaxSpeed = 15;
 
+    // Boost
     private float boost;
 
+    // Jump
+    private bool isJumping;
+    private float hVelocity;
+    private float height;
+    private const float MAX_HEIGHT = 1;
+    private const float GRAVITY = 6f;
     // Drift
     private bool isDrifting;
     private float driftAngle;
     private float driftCharge;
+    private const float DRIFT_STRENGTH = 48;
 
+    // Other
     private Rigidbody2D rb;
-
+    private SpriteRenderer spriteRenderer;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         float xMove = Input.GetAxisRaw("Horizontal");
         float yMove = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(KeyCode.Space)) Jump(2f);
 
         if (Input.GetKey(KeyCode.LeftShift))
         {
@@ -59,9 +71,9 @@ public class Racer : MonoBehaviour
 
         if (isDrifting)
         {
-            newAcceleration *= 1.6f;
+            newAcceleration *= 1.4f;
 
-            Vector2 driftMove = Quaternion.AngleAxis(45 * driftAngle, Vector3.forward) * transform.right;
+            Vector2 driftMove = Quaternion.AngleAxis(DRIFT_STRENGTH * driftAngle, Vector3.forward) * transform.right;
             move += driftMove;
 
             driftCharge += Time.deltaTime / 4;
@@ -81,6 +93,26 @@ public class Racer : MonoBehaviour
         }
 
         LookFoward();
+        if (isJumping)UpdateHeight();
+    }
+
+
+    public void UpdateHeight()
+    {
+        height += hVelocity * Time.deltaTime;
+        if (height > MAX_HEIGHT) height = MAX_HEIGHT;
+
+        hVelocity -= GRAVITY * Time.deltaTime;
+
+        transform.localScale = (height+1) * Vector2.one;
+
+        if (height <= 0)
+        {
+            height = 0;
+            transform.localScale = Vector2.one;
+            hVelocity = 0;
+            isJumping = false;
+        }
     }
 
     private void LookFoward()
@@ -97,5 +129,12 @@ public class Racer : MonoBehaviour
         boost += b;
 
         rb.velocity = rb.velocity.normalized * (baseMaxSpeed + boost);
+    }
+
+    public void Jump(float h)
+    {
+        if (isJumping) return;
+        isJumping = true;
+        hVelocity = h;
     }
 }
