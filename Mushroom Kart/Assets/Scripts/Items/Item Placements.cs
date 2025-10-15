9194db804fea29ab8b96c66b0c2e5f83eb50fa89
Andrewpiza <148090873+Andrewpiza@users.noreply.github.com>
2025-10-15 18:40:38 -0400

    Item Placement

---
+++ Mushroom Kart/Assets/Scripts/Items/Item Placements.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item Placement", menuName = "ScriptableObjects/Item Placement")]
+public class ItemPlacements : ScriptableObject
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
